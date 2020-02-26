f616ac3298b1e90df3c65c8c9196481d3a22bf75
basel-shehabi <baselshehabi1@gmail.com>
2020-02-26 23:36:45 +0000

    Completed Chapter 6 with Exercises. An error is given during testing of the chapter saying it can't find the hyperlink or find the markup when adding a test category. Both of these were included and I'm unaware what could be the issue. The source code can be verifiable by loading index.html and inspecting it.

--- populate_rango.py
+++ populate_rango.py
@@ -15,25 +15,33 @@ def populate():
 
 	python_pages = [
 		{'title': 'Official Python Tutorial',
-		 'url':'http://docs.python.org/3/tutorial/'},
+		 'url':'http://docs.python.org/3/tutorial/',
+		 'views': '60' },
 		{'title':'How to Think like a Computer Scientist',
-		 'url':'http://www.greenteapress.com/thinkpython/'},
+		 'url':'http://www.greenteapress.com/thinkpython/',
+		 'views': '55' },
 		{'title':'Learn Python in 10 Minutes',
-		 'url':'http://www.korokithakis.net/tutorials/python/'} ]
+		 'url':'http://www.korokithakis.net/tutorials/python/',
+		 'views': '50' } ]
 
 	django_pages = [
 		{'title':'Official Django Tutorial',
-		 'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+		 'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+		 'views': '45' },
 		{'title':'Django Rocks',
-		 'url':'http://www.djangorocks.com/'},
+		 'url':'http://www.djangorocks.com/',
+		 'views': '40' },
 		{'title':'How to Tango with Django',
-		 'url':'http://www.tangowithdjango.com/'} ]
+		 'url':'http://www.tangowithdjango.com/',
+		 'views': '35' } ]
 
 	other_pages = [
 		{'title':'Bottle',
-		 'url':'http://bottlepy.org/docs/dev/'},
+		 'url':'http://bottlepy.org/docs/dev/',
+		 'views': '15' },
 		{'title':'Flask',
-		 'url':'http://flask.pocoo.org'} ]
+		 'url':'http://flask.pocoo.org',
+		 'views': '20'} ]
 
 	cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
 			'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
@@ -47,14 +55,14 @@ def populate():
 	for cat, cat_data in cats.items():
 		c = add_cat(cat, cat_data['views'], cat_data['likes'])
 		for p in cat_data['pages']:
-			add_page(c, p['title'], p['url'])
+			add_page(c, p['title'], p['url'], p['views'])
 
 	# Print out the categories we have added.
 	for c in Category.objects.all():
 		for p in Page.objects.filter(category=c):
 			print(f'- {c}: {p}')
 
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views):
 	p = Page.objects.get_or_create(category=cat, title=title)[0]
 	p.url=url
 	p.views=views
